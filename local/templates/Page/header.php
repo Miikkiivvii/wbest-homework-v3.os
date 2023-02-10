91559f6932927cb9d17fa3e971422c70f3ce8bdf
Егор Самсонкин <miikkiivvii@gmail.com>
2023-02-10 11:40:57 +0300

    optimization header|footer

--- local/templates/Page/header.php
+++ local/templates/Page/header.php
@@ -22,73 +22,7 @@ IncludeTemplateLangFile(__FILE__);
 <?$APPLICATION->ShowPanel();?>
 	<div class="wrap">
 		<div class="hd_header_area">
-			<div class="hd_header">
-				<table>
-					<tr>
-						<td rowspan="2" class="hd_companyname">
-							<h1><a href=""><?=GetMessage('Furniture_store')?></a></h1>
-						</td>
-						<td rowspan="2" class="hd_txarea">
-						<?$APPLICATION->IncludeComponent(
-							"bitrix:main.include",
-							"",
-							Array(
-								"AREA_FILE_SHOW" => "file",
-								"AREA_FILE_SUFFIX" => "inc",
-								"EDIT_TEMPLATE" => "",
-								"PATH" => "/local/templates/Page/components/header-phone.php"
-							)
-						);?>
-						<td style="width:232px">
-							<form action="">
-								<div class="hd_search_form" style="float:right;">
-									<input placeholder="<?=GetMessage('Search')?>" type="text"/>
-									<input type="submit" value=""/>
-								</div>
-							</form>
-						</td>
-					</tr>
-					<tr>
-						<td style="padding-top: 11px;">
-							<span class="hd_singin"><a id="hd_singin_but_open" href=""><?=GetMessage('Sign_in')?></a>
-							<div class="hd_loginform">
-								<span class="hd_title_loginform"><?=GetMessage('Sign_in')?></span>
-								<form name="" method="" action="">
-					
-									<input placeholder="Логин"  type="text">
-									<input  placeholder="Пароль"  type="password">			
-									<a href="/" class="hd_forgotpassword"><?=GetMessage('Forgot_your_password')?></a>
-									
-									<div class="head_remember_me" style="margin-top: 10px">
-										<input id="USER_REMEMBER_frm" name="USER_REMEMBER" value="Y" type="checkbox">
-										<label for="USER_REMEMBER_frm" title="Запомнить меня на этом компьютере"><?=GetMessage('Remember_me')?></label>
-									</div>				
-									<input value="Войти" name="Login" style="margin-top: 20px;" type="submit">
-									</form>
-								<span class="hd_close_loginform"><?=GetMessage('close')?></span>
-							</div>
-							</span><br>
-							<a href="" class="hd_signup"><?=GetMessage('Register')?></a>
-						</td>
-					</tr>
-				</table>
-				<?$APPLICATION->IncludeComponent("bitrix:menu", "top_multi", Array(
-	"ALLOW_MULTI_SELECT" => "N",	// Разрешить несколько активных пунктов одновременно
-		"CHILD_MENU_TYPE" => "left",	// Тип меню для остальных уровней
-		"DELAY" => "N",	// Откладывать выполнение шаблона меню
-		"MAX_LEVEL" => "2",	// Уровень вложенности меню
-		"MENU_CACHE_GET_VARS" => array(	// Значимые переменные запроса
-			0 => "",
-		),
-		"MENU_CACHE_TIME" => "3600",	// Время кеширования (сек.)
-		"MENU_CACHE_TYPE" => "N",	// Тип кеширования
-		"MENU_CACHE_USE_GROUPS" => "Y",	// Учитывать права доступа
-		"ROOT_MENU_TYPE" => "top",	// Тип меню для первого уровня
-		"USE_EXT" => "N",	// Подключать файлы с именами вида .тип_меню.menu_ext.php
-	),
-	false
-);?>
-			</div>
+		<?include_once($_SERVER['DOCUMENT_ROOT'] . "/local/templates/.default/include/header.php")?>
 		</div>
 		<div class="bc_breadcrumbs">
 		<ul>
